82d3990f954baa379cc0a922c0816336123dbbd5
brovko.dr <brovko@atb.su>
2024-11-11 19:57:53 +1000

    initial conditions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import style from "./app.module.css";
 
 function App() {
+  // const { data, isLoading, error, refetch } = useFetch(
+  //   "https://jsonplaceholder.typicode.com/posts"
+  // );
+  const data = [];
+  const isLoading = false;
+  const error = false;
+  const refetch = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={style.wrapper}>
+      <div className={style.buttons}>
+        <button
+          onClick={() =>
+            refetch({
+              params: {
+                _limit: 3,
+              },
+            })
+          }
         >
-          Learn React
-        </a>
-      </header>
+          Перезапросить
+        </button>
+      </div>
+      {isLoading && "Загрузка..."}
+      {error && "Произошла ошибка"}
+      {data &&
+        !isLoading &&
+        data?.map((item, i) => (
+          <div key={item.id}>{`${i + 1}) ${item.title}`}</div>
+        ))}
     </div>
   );
 }
