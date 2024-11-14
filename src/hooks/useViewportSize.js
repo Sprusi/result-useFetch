3ba3e1808794d240d99b9e586e6c36f630efc60d
brovko.dr <brovko@atb.su>
2024-11-15 04:15:36 +1000

    fix useViewportSize

--- src/hooks/useViewportSize.js
+++ src/hooks/useViewportSize.js
@@ -1,33 +1,18 @@
-import { useEffect, useState } from "react";
-import { useThrottle } from "./useThottle";
+import { useState } from "react";
+import { useWindowEvent } from "./useWindowEvent";
 
-export const useViewportSize = (type, listener, options) => {
-  const eventType = type || "resize";
-  const eventFunction = listener || handleResize;
+export function useViewportSize() {
+  const [size, setSize] = useState({
+    width: typeof window !== "undefined" ? window.innerWidth : 0,
+    height: typeof window !== "undefined" ? window.innerHeight : 0,
+  });
 
-  const isWindow = typeof window !== "undefined";
-  const isResizeEvent = eventType === "resize";
-  const initialHeight = isWindow && isResizeEvent ? window.innerHeight : null;
-  const initialWidth = isWindow && isResizeEvent ? window.innerWidth : null;
+  useWindowEvent("resize", () => {
+    setSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  });
 
-  const [height, setHeight] = useState(initialHeight);
-  const [width, setWidth] = useState(initialWidth);
-
-  const thottleHeight = useThrottle(height);
-  const thottleWidth = useThrottle(width);
-
-  function handleResize() {
-    setHeight(window.innerHeight);
-    setWidth(window.innerWidth);
-  }
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener(eventType, eventFunction, options);
-      return () =>
-        window.removeEventListener(eventType, eventFunction, options);
-    }
-  }, [type, listener]);
-
-  return { height: thottleHeight, width: thottleWidth };
-};
+  return { height: size.height, width: size.width };
+}
